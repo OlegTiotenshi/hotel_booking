d75326de6e9e863791a70bd785e5e689eb24b524
oleg <tiotenshi2901@gmail.com>
2025-08-18 20:47:03 +0300

    add hotels to DB in API endpoint

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,7 +1,11 @@
 from fastapi import Query, APIRouter, Body
 
-from src.schemas.hotels import Hotel, HotelPATCH
+from sqlalchemy import insert
+
+from src.database import async_session_maker
 from src.api.dependencies import PaginationDep
+from src.schemas.hotels import Hotel, HotelPATCH
+from src.models.hotels import HotelsOrm
 
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
@@ -40,34 +44,31 @@ def get_hotels(
 
 
 @router.post("")
-def create_hotel(
+async def create_hotel(
     hotel_data: Hotel = Body(
         openapi_examples={
             "1": {
                 "summary": "Сочи",
                 "value": {
                     "title": "Отель Сочи 5 звезд у моря",
-                    "name": "sochi_u_morya",
+                    "location": "ул. Моря, 1",
                 },
             },
             "2": {
                 "summary": "Дубай",
                 "value": {
                     "title": "Отель Дубай все включено",
-                    "name": "dubai_all_inclusive",
+                    "location": "ул. Шейха, 2",
                 },
             },
         }
     )
 ):
-    global hotels
-    hotels.append(
-        {
-            "id": hotels[-1]["id"] + 1,
-            "title": hotel_data.title,
-            "name": hotel_data.name,
-        }
-    )
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
+
     return {"status": "OK"}
 
 
