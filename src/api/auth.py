c54b9774f2e4e78a207e4e37dda89c794bdf839b
oleg <tiotenshi2901@gmail.com>
2025-08-20 14:22:42 +0300

    add /auth/login route

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from passlib.context import CryptContext
 from sqlalchemy.exc import IntegrityError
+from datetime import datetime, timezone, timedelta
+
+import jwt
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -9,6 +12,21 @@ from src.schemas.users import UserRequestAdd, UserAdd
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode |= {"exp": expire}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.post("/register")
@@ -26,3 +44,25 @@ async def register_user(
             if "unique" in str(e).lower() and "email" in str(e).lower():
                 return {"status": "User with this email already exists"}
             return None
+
+
+@router.post("/login")
+async def login_user(
+    data: UserRequestAdd,
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(
+            email=data.email
+        )
+        if not user:
+            raise HTTPException(
+                status_code=401,
+                detail="Пользователь с таким email не зарегистрирован",
+            )
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(
+                status_code=401,
+                detail="Пароль неверный",
+            )
+        access_token = create_access_token({"user_id": user.id})
+        return {"access_token": access_token}
