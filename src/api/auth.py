c2bc4e6b93dcae38e9ebab0b5e44fbc3b2a0c0eb
oleg <tiotenshi2901@gmail.com>
2025-08-20 15:06:35 +0300

    temp route

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Request
 from sqlalchemy.exc import IntegrityError
 
 
@@ -49,3 +49,11 @@ async def login_user(
         access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+
+@router.get("/only_auth")
+async def only_auth(
+    request: Request,
+):
+    cookies = request.cookies
+    access_token = request.cookies.get("access_token")
