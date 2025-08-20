63ed75d8138ceed76c0524606b51f5732bbe48aa
oleg <tiotenshi2901@gmail.com>
2025-08-20 15:40:08 +0300

    create UserIdDep

--- src/services/auth.py
+++ src/services/auth.py
@@ -38,3 +38,5 @@ class AuthService:
             )
         except jwt.exceptions.DecodeError:
             raise HTTPException(status_code=401, detail="Неверный токен")
+        except jwt.exceptions.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail="Токен истек")
