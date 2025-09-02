069d2dfef91c14ded8d30335a123b0cb97582b2b
oleg <tiotenshi2901@gmail.com>
2025-09-02 23:58:07 +0300

    add facilities models

---
+++ src/migrations/versions/2025_09_02_2356-03411c93d4c1_add_facilities.py
@@ -0,0 +1,47 @@
+"""add facilities
+
+Revision ID: 03411c93d4c1
+Revises: 732bc50f9c53
+Create Date: 2025-09-02 23:56:45.585631
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "03411c93d4c1"
+down_revision: Union[str, Sequence[str], None] = "732bc50f9c53"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("rooms_facilities")
+    op.drop_table("facilities")
