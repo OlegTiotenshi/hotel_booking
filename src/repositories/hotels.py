1204b0335fa63aa38be00fb7c7e1e091fa3f4fc9
oleg <tiotenshi2901@gmail.com>
2025-09-02 21:58:12 +0300

    create get_filtered_by_time() for HotelsRepository

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,7 +1,12 @@
+from datetime import date
+
+from sqlalchemy import select, func
+
 from src.schemas.hotels import Hotel
 from src.repositories.base import BaseRepository
 from src.models.hotels import HotelsOrm
-from sqlalchemy import select
+from src.models.rooms import RoomsOrm
+from src.repositories.utils import rooms_ids_for_booking
 
 
 class HotelsRepository(BaseRepository):
@@ -28,3 +33,16 @@ class HotelsRepository(BaseRepository):
             Hotel.model_validate(hotel, from_attributes=True)
             for hotel in result.scalars().all()
         ]
+
+    async def get_filtered_by_time(
+        self,
+        date_from: date,
+        date_to: date,
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+        return await self.get_all_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
