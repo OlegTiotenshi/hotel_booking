608c0716693f1018537598b117a75f5665a9a0fb
oleg <tiotenshi2901@gmail.com>
2025-09-03 18:53:46 +0300

    create DataMapper and HotelMapper

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from sqlalchemy import delete, insert, select, update
 
+from src.repositories.mappers.base import DataMapper
+
 
 class BaseRepository:
     model = None
-    schema: BaseModel = None
+    mapper: DataMapper = None
 
     def __init__(self, session):
         self.session = session
@@ -13,7 +15,7 @@ class BaseRepository:
         query = select(self.model).filter(*filter).filter_by(**filter_by)
         result = await self.session.execute(query)
         return [
-            self.schema.model_validate(model, from_attributes=True)
+            self.mapper.map_to_domain_entity(model)
             for model in result.scalars().all()
         ]
 
@@ -26,13 +28,13 @@ class BaseRepository:
         model = result.scalars().one_or_none()
         if model is None:
             return None
-        return self.schema.model_validate(model, from_attributes=True)
+        return self.mapper.map_to_domain_entity(model)
 
     async def add(self, data: BaseModel):
         stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         result = await self.session.execute(stmt)
         model = result.scalars().one()
-        return self.schema.model_validate(model, from_attributes=True)
+        return self.mapper.map_to_domain_entity(model)
 
     async def add_bulk(self, data: list[BaseModel]):
         stmt = insert(self.model).values([item.model_dump() for item in data])
